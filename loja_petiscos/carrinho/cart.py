51f29c173f3299d703e39bb7ddb40593e754add6
godbernaz <bernazbarros2000@gmail.com>
2024-12-14 14:29:28 +0000

    testing merges

--- loja_petiscos/carrinho/cart.py
+++ loja_petiscos/carrinho/cart.py
@@ -1,3 +1,5 @@
+from loja.models import Product
+
 class Cart():
 	def __init__(self, request):
 		self.session = request.session
@@ -13,4 +15,36 @@ class Cart():
 
 		# Make sure cart is available on all pages of site
 		self.cart = cart
-        
+
+	def add(self, product):
+		product_id = str(product.id)
+		#product_qty = str(quantity)
+		
+		if product_id in self.cart:
+			pass
+		else:
+			self.cart[product_id] = {'price': str(product.price)}
+			#self.cart[product_id] = int(product_qty)
+
+		self.session.modified = True
+
+		# Deal with logged in user
+		#if self.request.user.is_authenticated:
+			# Get the current user profile
+			#current_user = Profile.objects.filter(user__id=self.request.user.id)
+			# Convert {'3':1, '2':4} to {"3":1, "2":4}
+			#carty = str(self.cart)
+			#carty = carty.replace("\'", "\"")
+			# Save carty to the Profile Model
+			#current_user.update(old_cart=str(carty))
+   
+	def __len__(self):
+		return len(self.cart)
+
+	def get_prods(self):
+		product_ids = self.cart.keys()
+  
+		products = Product.objects.filter(id__in=product_ids)
+  
+		return products
+
