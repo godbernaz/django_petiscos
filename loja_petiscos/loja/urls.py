8a7c25fb70e10b97b556955574316832f37153e6
godbernaz <bernazbarros2000@gmail.com>
2024-12-14 19:27:26 +0000

    searching bar incoming today, tomorrow i'll finish everything before I use PostGres cloud database

--- loja_petiscos/loja/urls.py
+++ loja_petiscos/loja/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('inciar-sessao/', views.login_user, name='login'),
     path('terminar-sessao/', views.logout_user, name='logout'),
     path('registar/', views.register_user, name='register'),
+    path('editar_password/', views.update_password, name='update_password'),
+    path('editar_perfil/', views.update_user, name='update_user'),
+    path('editar_info/', views.update_info, name='update_info'),
     path('produto/<int:pk>', views.about_product, name='about_product'),
     path('categoria/<str:foo>', views.category, name='category'),
 ]
--- loja_petiscos/loja/views.py
+++ loja_petiscos/loja/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
-from .models import Product, Category
+from .models import Product, Category, Profile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
 from django import forms
 
 def home(request):
@@ -47,13 +47,76 @@ def register_user(request):
             
             user = authenticate(username=username, password=password)
             login(request, user)
-            messages.success(request, ("A tua conta foi criada com sucesso!."))
-            return redirect('home')
+            messages.success(request, ("Conta criada com sucesso, se quiseres termina de atualizar o teu perfil!."))
+            return redirect('update_info')
         else:
             messages.error(request, ("Houve um problema com a criação da tua conta. Tenta novamente."))
             return redirect('register')
     else:
         return render(request, 'register.html', {'form':form})
+
+def update_user(request):
+    
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        user_form = UpdateUserForm(request.POST or None, instance=current_user)
+        
+        if user_form.is_valid():
+            user_form.save()
+        
+            login(request, current_user)
+            messages.success(request, ("O seu perfil foi atualizado!."))
+            return redirect('home')
+        
+        return render(request, 'update_user.html', {'user_form':user_form})
+    
+    else:
+        messages.error(request, ("Tens de iniciar sessão para alterares o teu perfil."))
+        return redirect('home')
+    
+def update_info(request):
+    
+    if request.user.is_authenticated:
+        current_user = Profile.objects.get(user__id=request.user.id)
+        form = UserInfoForm(request.POST or None, instance=current_user)
+        
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("O as informações do teu perfil foram atualizadas!."))
+            return redirect('home')
+        
+        return render(request, 'update_info.html', {'form':form})
+    else:
+        messages.success(request, ("Tens de iniciar sessão para alterares as informações do teu perfil."))
+        return redirect('home')
+        
+            
+
+
+
+def update_password(request):
+    
+    if request.user.is_authenticated:
+        current_user = request.user
+        
+        if request.method == 'POST':
+            form = ChangePasswordForm(current_user, request.POST)
+            
+            if form.is_valid():
+                form.save()
+                messages.success(request, "A tua password foi alterada!.")
+                login(request, current_user)
+                return redirect('update_user')
+            else:
+                for error in list(form.errors.values()):
+                    messages.error(request, error)
+                return redirect('update_password')
+        else:
+            form = ChangePasswordForm(current_user)
+            return render(request, "update_password.html", {'form': form})
+    else:
+        messages.success(request, "Tens de entrar na tua conta para veres isto!.")
+        return redirect('home')
 
 def about_product(request, pk):
     product = Product.objects.get(id=pk)
@@ -68,3 +131,4 @@ def category(request, foo):
     except:
         messages.error(request, ("A categoria que procuras não tem produtos disponiveis"))
         return redirect('home')
+    
