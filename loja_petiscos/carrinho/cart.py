2496ffa714a4d4561b6052e0ad7debca759aee6c
godbernaz <bernazbarros2000@gmail.com>
2024-12-14 15:46:07 +0000

    almost finishing the basics, payment, shipping address, and admin dashboard needed

--- loja_petiscos/carrinho/cart.py
+++ loja_petiscos/carrinho/cart.py
@@ -16,15 +16,15 @@ class Cart():
 		# Make sure cart is available on all pages of site
 		self.cart = cart
 
-	def add(self, product):
+	def add(self, product, quantity):
 		product_id = str(product.id)
-		#product_qty = str(quantity)
+		product_qty = str(quantity)
 		
 		if product_id in self.cart:
 			pass
 		else:
-			self.cart[product_id] = {'price': str(product.price)}
-			#self.cart[product_id] = int(product_qty)
+			#self.cart[product_id] = {'price': str(product.price)}
+			self.cart[product_id] = int(product_qty)
 
 		self.session.modified = True
 
@@ -43,8 +43,49 @@ class Cart():
 
 	def get_prods(self):
 		product_ids = self.cart.keys()
-  
 		products = Product.objects.filter(id__in=product_ids)
-  
 		return products
 
+	def get_quants(self):
+		quantities = self.cart
+		return quantities
+
+	def update(self, product, quantity):
+		product_id = str(product)
+		product_qty = int(quantity)
+  
+		ourcart = self.cart
+  
+		ourcart[product_id] = product_qty
+  
+		self.session.modified = True
+  
+		thing = self.cart
+		return thing
+
+	def delete(self, product):
+		product_id = str(product)
+  
+		if product_id in self.cart:
+			del self.cart[product_id]
+   
+		self.session.modified = True
+
+	def cart_total(self):
+		product_ids = self.cart.keys()
+		products = Product.objects.filter(id__in=product_ids)
+		quantities = self.cart
+		total = 0
+  
+		for key, value in quantities.items():
+			key = int(key)
+			for product in products:
+				if product.id == key:
+					if product.is_sale:
+						total = total + (product.sale_price * value)
+					else:
+						total = total + (product.price * value)
+     
+     
+		
+		return total
